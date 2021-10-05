566861f60d0b7876fbd5900e5356d63ebb8f4dc6
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-10-05 21:24:11 +0300

    add action creator

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
 import { createStore } from 'redux';
-
-// init with 0 if state is undefined
-const reducer = (state = 0, action) => {
-    switch (action.type){
-        case 'RND':
-            return state + action.payload;
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        default:
-            return state;
-    }
-};
+import reducer from './reducer';
+import { inc, dec, rnd} from './actions';
 
 const store = createStore(reducer);
+
 document
     .getElementById('inc')
     .addEventListener('click', () => {
-        store.dispatch({type: 'INC'});
+        store.dispatch(inc());
     });
 
 document
     .getElementById('dec')
     .addEventListener('click', () => {
-        store.dispatch({type: 'DEC'});
+        store.dispatch(dec());
     });
 
 document
     .getElementById('rnd')
     .addEventListener('click', () => {
         const payload = Math.floor(Math.random() * 10);
-        store.dispatch({
-            type: 'RND',
-            payload
-        });
+        store.dispatch(rnd(payload));
     });
 
 const update = () => {
---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+// init with 0 if state is undefined
+const reducer = (state = 0, action) => {
+
+    switch (action.type) {
+        case 'RND':
+            return state + action.payload;
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+export default reducer;
