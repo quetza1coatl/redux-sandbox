882b48f037b5d01124ae77d9956b92e00770448d
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-10-05 21:09:44 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 // init with 0 if state is undefined
 const reducer = (state = 0, action) => {
     switch (action.type){
@@ -6,12 +8,13 @@ const reducer = (state = 0, action) => {
         default:
             return state;
     }
-}
+};
 
-let state = reducer(undefined, {});
-
-state = reducer(state, {type:'INC'});
-console.log(state);
-
-state = reducer(state, {type:'INC'});
-console.log(state);
+const store = createStore(reducer);
+store.subscribe( () => {
+   console.log(store.getState());
+});
+// console.log(store.getState());
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+// console.log(store.getState());
